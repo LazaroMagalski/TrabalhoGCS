2d8703a945429ed3387659c25fcc2ffcbeb5eac3
kaitowing <arthurpizzolatti@gmail.com>
2022-09-09 18:54:39 -0300

    Apenas função

--- src/main/java/com/trabalho/App.java
+++ src/main/java/com/trabalho/App.java
@@ -1,29 +1,10 @@
 package com.trabalho;
-import java.util.*;
 public class App 
 {
-    public int identifyingTea()
+    public int identifyingTea(int opt, int tea)
     {
-        Scanner in = new Scanner(System.in);
-        int opt,tea,num = 0;
-
-        System.out.println("Insert the right tea");
-        System.out.println("1- White tea");
-        System.out.println("2- Green tea");
-        System.out.println("3- Black tea");
-        System.out.println("4- Herbal tea");
-        tea = in.nextInt();
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Insert your Answer!");
-            System.out.println("1- White tea");
-            System.out.println("2- Green tea");
-            System.out.println("3- Black tea");
-            System.out.println("4- Herbal tea");
-            opt = in.nextInt();
-            
-            if(opt == tea) num++;
-        }
+        int num = 0;
+        if(opt == tea) num++;
         return num;
     }
 }
