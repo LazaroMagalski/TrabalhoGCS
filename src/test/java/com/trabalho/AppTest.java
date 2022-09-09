b3e6cbfac2182c80da35353e62759830680a5874
Marcus Filipe Quirino <marcus@Marcuss-MacBook-Air-2.local>
2022-09-09 18:57:51 -0300

    adicionado testes do método identifyingTea()

--- src/test/java/com/trabalho/AppTest.java
+++ src/test/java/com/trabalho/AppTest.java
@@ -1,20 +1,19 @@
 package com.trabalho;
-
-import static org.junit.Assert.assertTrue;
+ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void testIdentifyingTea() {
+        App app = new App();
+        int result = app.identifyingTea(3, 3);
+        assertEquals(1, result);
+    }
+
+    @Test
+    void testIdentifyingTea2() {
+        App app = new App();
+        int result = app.identifyingTea(3, 2);
+        assertEquals(0, result);
     }
 }
