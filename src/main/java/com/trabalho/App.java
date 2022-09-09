c52e11649e89ae15f45bd4488e30644ad8996ad3
kaitowing <arthurpizzolatti@gmail.com>
2022-09-09 18:43:10 -0300

    Código finalizado - Programa pronto.

--- src/main/java/com/trabalho/App.java
+++ src/main/java/com/trabalho/App.java
@@ -1,8 +1,29 @@
 package com.trabalho;
+import java.util.*;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Teste!" );
+        Scanner in = new Scanner(System.in);
+        int opt,tea,num = 0;
+
+        System.out.println("Insert the right tea");
+        System.out.println("1- White tea");
+        System.out.println("2- Green tea");
+        System.out.println("3- Black tea");
+        System.out.println("4- Herbal tea");
+        tea = in.nextInt();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Insert your Answer!");
+            System.out.println("1- White tea");
+            System.out.println("2- Green tea");
+            System.out.println("3- Black tea");
+            System.out.println("4- Herbal tea");
+            opt = in.nextInt();
+            
+            if(opt == tea) num++;
+        }
+        System.out.println("Number of right answers!: " + num);
     }
 }
