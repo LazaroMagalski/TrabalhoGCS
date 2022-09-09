d4d42871d85d4b84d46e733246071244d5e5c0e1
kaitowing <arthurpizzolatti@gmail.com>
2022-09-09 18:21:34 -0300

    Teste maven

--- src/main/java/com/trabalho/App.java
+++ src/main/java/com/trabalho/App.java
@@ -1,13 +1,8 @@
 package com.trabalho;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Teste!" );
     }
 }
