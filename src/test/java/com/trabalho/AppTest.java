7de227bbae1e5f882cad9795beb6e6c7579db15e
Marcelo <65903875+marceloDomper@users.noreply.github.com>
2022-09-12 18:06:01 -0300

    update metodos

--- src/test/java/com/trabalho/AppTest.java
+++ src/test/java/com/trabalho/AppTest.java
@@ -2,16 +2,16 @@ package com.trabalho;
  import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-class AppTest {
+public class AppTest {
     @Test
-    void testIdentifyingTea() {
+    public void testIdentifyingTea() {
         Tea app = new Tea();
         int result = app.identifyingTea(3, 3);
         assertEquals(1, result);
     }
 
     @Test
-    void testIdentifyingTea2() {
+    public void testIdentifyingTea2() {
         Tea app = new Tea();
         int result = app.identifyingTea(3, 2);
         assertEquals(0, result);
